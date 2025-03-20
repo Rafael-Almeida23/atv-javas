aefa61d62b1dbd92e098e8046c883740085f37cc
Rafael-Almeida23 <rafael_a_de-mello@estudante.sesisenai.org.br>
2025-03-20 09:47:50 -0300

    Atividade 2

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 console.log('Hello World MotherFuckers');
 
-let numero = prompt("Dígite seu número");
+let n1 = parseFloat(prompt("Escreva o primeiro número:"));
+let n2 = parseFloat(prompt("Escreva o segundo número:"));
 
-function verificarPar(numero){
-
-    if (numero % 2 === 0) {
-       console.log(numero + " é igual a par.");
+function calcular(num1,num2){
+    const soma = num1 + num2;
+    const subtracao = num1 - num2;
+    const divisao = num1 / num2;
+    const multiplicacao = num1 * num2;
+    console.log(soma)
+    console.log(subtracao)
+    console.log(multiplicacao)
+    if (num2 === 0) {
+        console.log("Não da pra dividir por 0")
     } else {
-        console.log(numero + " é igual a ímpar."); 
+    const divisao = num1 / num2;
+    console.log(divisao)
     }
 }
-verificarPar(numero);
+
+calcular(n1, n2);
