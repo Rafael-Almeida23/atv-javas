d20c5f02532b6dbf9777ce0612b6508921040b5e
Rafael-Almeida23 <rafael_a_de-mello@estudante.sesisenai.org.br>
2025-03-24 20:03:28 -0300

    atividade 7

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 console.log("Hello World Motherfuckers")
+ 
+function mensagemPersonalizada() {
+    let nome = prompt("Digite seu nome:");
+    let mensagem = prompt("Digite sua mensagem (ou deixe em branco para usar 'Olá!'):") || "Olá!";
+    
+    console.log("Olá!"); 
+    console.log(`${mensagem} ${nome}`); // ${} Permite inserir variáveis e expressões diretamente dentro da string.
+}
 
-const carro = Object(); // object é uma entidade independente com propiedades
-carro.fabricacao = 'Corvette';
-carro.modelo = 'C8';
-carro.ano = '2020';
-
-console.log(carro.modelo); // isso faz com que apenas o modelo apareça no console
+mensagemPersonalizada(); // Chama a função quando a página for carregada
