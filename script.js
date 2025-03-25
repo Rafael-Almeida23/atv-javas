944c3259a1d84ef3411b95e829288c6d3e084af4
Rafael-Almeida23 <rafael_a_de-mello@estudante.sesisenai.org.br>
2025-03-24 21:20:57 -0300

    atividade 16

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 console.log("Hello World Motherfuckers")
 
-let texto = prompt("Digite uma frase:");
-function verificarTamanho(texto) {
-    if (texto.length > 10) {
-        console.log("A string tem mais de 10 caracteres.");
-    } else {
-        console.log("A string tem 10 caracteres ou menos.");
-    }
-}
-// O .length conta quantos caracteres tem uma palavra ou quantos itens tem uma lista.
-verificarTamanho(texto);
+let frutas = ["maçã", "banana", "laranja", "manga", "uva"];
+console.log(frutas[2]);  // A terceira fruta está no 2, pois 0 conta como algarismo
