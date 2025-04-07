3b1b85933b0382785031de262ab70055b9f44c2b
Rafael-Almeida23 <rafael_a_de-mello@estudante.sesisenai.org.br>
2025-04-07 09:30:19 -0300

    Atv 5 - Revisão

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 console.log("Hello World Motherfuckers");
 
-let numeros = [];
+let numero = parseInt(prompt("Digite um número:"));
 
-for (let i = 1; i <= 4; i++) {
-    let valor = parseInt(prompt(`Digite o ${i}º número inteiro:`));
-    numeros.push(valor);
+if (numero % 2 === 0) {
+    // Número é par, então transformamos em ímpar somando 1
+    numero += 1;
+    alert(`O número era par, Agora é ímpar: ${numero}`);
+} else {
+    // Número é ímpar, transformamos em par subtraindo 1
+    numero -= 1;
+    alert(`O número era ímpar, Agora é par: ${numero}`);
 }
-
-numeros.sort((a, b) => b - a); // Ordena em ordem decrescente
-
-alert("Números em ordem decrescente:\n" + numeros.join(", "));
